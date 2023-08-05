725808e1bffb6715b067cd4c7b6d1a5f1f5b6917
Ilya Kozirskiy <ilyadevkoz@gmail.com>
2023-08-05 23:30:48 +0300

    start

---
+++ app/src/main/java/com/ilyakoz/notes/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ilyakoz.notes.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ilyakoz.notes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
