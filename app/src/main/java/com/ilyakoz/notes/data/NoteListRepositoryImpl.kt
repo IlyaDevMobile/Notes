1b48d9c9b51c0b87dd40ad37dc67262fc2b0b810
Ilya Kozirskiy <ilyadevkoz@gmail.com>
2023-08-07 00:59:14 +0300

    created presentation

--- app/src/main/java/com/ilyakoz/notes/data/NoteListRepositoryImpl.kt
+++ app/src/main/java/com/ilyakoz/notes/data/NoteListRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.ilyakoz.notes.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.ilyakoz.notes.domain.NoteItem
 import com.ilyakoz.notes.domain.NoteListRepository
 
@@ -7,7 +9,16 @@ object NoteListRepositoryImpl : NoteListRepository {
 
     private val noteList = mutableListOf<NoteItem>()
 
+    private val noteListLd = MutableLiveData<List<NoteItem>>()
+
     private var autoIncrementId = 0
+
+    init {
+        for (i in 0 until 10){
+            val item = NoteItem("Название $i", "описание $i",true )
+            addNoteItem(item)
+        }
+    }
 
 
     override fun addNoteItem(noteItem: NoteItem) {
@@ -15,10 +26,12 @@ object NoteListRepositoryImpl : NoteListRepository {
             noteItem.id = autoIncrementId++
         }
         noteList.add(noteItem)
+        updateList()
     }
 
     override fun deleteNoteItem(noteItem: NoteItem) {
         noteList.remove(noteItem)
+        updateList()
     }
 
     override fun editNoteItem(noteItem: NoteItem) : Boolean {
@@ -37,7 +50,11 @@ object NoteListRepositoryImpl : NoteListRepository {
         } ?: throw NoSuchElementException("Element with id $noteItemId not found")
     }
 
-    override fun getNoteList(): List<NoteItem> {
-        return noteList.toList()
+    override fun getNoteList(): LiveData<List<NoteItem>> {
+        return noteListLd
+    }
+
+    private fun updateList(){
+        noteListLd.value = noteList.toList()
     }
 }
